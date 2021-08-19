0d4fb7f0ba88cef5986d223e203a92af597dae90
Juan Franco <juanferfranco@gmail.com>
2021-08-19 07:45:09 -0500

    add tasks setup

--- evalUnidad1.ino
+++ evalUnidad1.ino
@@ -1,9 +1,25 @@
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void task1(){
+
+  
+}
+
+void task2(){
+  
+}
+
+
+void task3(){
+  
+}
+
+
+void loop() {
+  task1();
+  task2();
+  task3();
 }
